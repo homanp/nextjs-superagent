de69fbb01d84affcc03b36bc0962c792bd5abf97
Ismail Pelaseyed <homanp@gmail.com>
2023-05-29 11:51:00 +0200

    Initial commit

--- app/page.js
+++ app/page.js
@@ -1,113 +1,118 @@
-import Image from 'next/image'
+"use client";
+import { useCallback, useState } from "react";
+import { BeatLoader } from "react-spinners";
+import { superagent } from "@/lib/superagent";
+import { useForm } from "react-hook-form";
 
 export default function Home() {
+  const [message, setMessage] = useState();
+  const [aiResponse, setAiResponse] = useState();
+  const {
+    formState: { isSubmitting, error },
+    handleSubmit,
+    register,
+  } = useForm();
+
+  const onSubmit = useCallback(async (values) => {
+    const { message } = values;
+
+    setMessage(message);
+    setAiResponse();
+
+    const { data } = await superagent.agents().predict({
+      id: process.env.NEXT_PUBLIC_AGENT_ID,
+      input: { question: message },
+      has_streaming: false,
+    });
+
+    setAiResponse(data);
+  }, []);
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex mx-auto min-h-screen flex-col justify-between p-24 max-w-5xl font-mono">
+      <div className="flex flex-col space-y-7">
+        <div className="z-10 w-full  mx-auto items-start justify-center font-mono text-md lg:flex">
+          <p className="flex w-full align-center justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+            Superagent.sh in NextJS
+          </p>
+        </div>
+
+        <div
+          class="p-2 items-center bg-indigo-800 items-center text-indigo-100 leading-none lg:rounded-full flex lg:inline-flex"
+          role="alert"
+        >
+          <span class="flex rounded-full bg-indigo-500 uppercase px-2 py-1 text-xs font-bold mr-3">
+            Demo
+          </span>
+          <span class="font-semibold mr-2 text-left flex-auto">
+            Ask questions to Teslas Q1 2023 Earnings Agent.
+          </span>
+        </div>
+
+        <div className="flex flex-col space-y-4">
+          {message && (
+            <div className="flex space-x-2 text-orange-500 font-bold text-md">
+              <div className="flex items-center space-x-2">
+                <p className="text-white">
+                  <svg
+                    class="fill-current opacity-75 h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                  </svg>
+                </p>
+                <p>{message}</p>
+                {isSubmitting && <BeatLoader color="white" size={6} />}
+              </div>
+            </div>
+          )}
+          {aiResponse && (
+            <div className="flex flex-col space-x-2 text-green-500 font-bold text-md">
+              <div className="flex items-start space-x-2">
+                <p className="text-white">
+                  <svg
+                    class="fill-current opacity-75 h-5 w-5"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M12.95 10.707l.707-.707L8 4.343 6.586 5.757 10.828 10l-4.242 4.243L8 15.657l4.95-4.95z" />
+                  </svg>
+                </p>
+                <p>{aiResponse}</p>
+              </div>
+            </div>
+          )}
         </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="sm:col-span-4">
+          <label
+            htmlFor="username"
+            className="block text-sm font-medium leading-6 text-gray-900"
+          >
+            Username
+          </label>
+          <div className="mt-2 flex justify-between space-x-2">
+            <div className="rounded-md flex flex-1 shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 ">
+              <input
+                disabled={isSubmitting}
+                type="text"
+                {...register("message", { required: true })}
+                className="block  w-full border-0 bg-transparent py-1.5 pl-2 text-gray-100 placeholder:text-gray-400 focus:ring-0 sm:text-md sm:leading-6"
+                placeholder="Enter your message..."
+              />
+            </div>
+            <button
+              disabled={isSubmitting}
+              class="bg-indigo-800 font-mono hover:bg-gray-700 text-white font-bold py-2 px-4 rounded text-md"
+            >
+              {isSubmitting ? <BeatLoader color="white" size={8} /> : "Send"}
+            </button>
+          </div>
+        </div>
+      </form>
     </main>
-  )
+  );
 }
